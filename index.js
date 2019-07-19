2852a1c286c7e0a78c577ad9b4f0e5ed36c14a6f
Francesco Lentini <francesco.lentini@gmail.com>
2019-07-19 15:42:48 +0100

    update eslint conf + prettier

--- index.js
+++ index.js
@@ -1,148 +1,163 @@
-const kafka = require('kafka-node');
-const debug = require('debug')('engine:kafka');
-const A = require('async');
-const _ = require('lodash');
+const kafka = require('kafka-node')
+const debug = require('debug')('engine:kafka')
+const A = require('async')
+const _ = require('lodash')
 
-const { log: logger } = console;
-const DEFAULT_MESSAGE_SIZE = 300;
+const { log: logger } = console
+const DEFAULT_MESSAGE_SIZE = 300
 
 const createProducer = (client, scenarioSpec) => {
-  const producer = scenarioSpec.some(scenario => scenario
-    .flow.some(({ publishMessage = {} }) => publishMessage && 'partition' in publishMessage));
+  const producer = scenarioSpec.some(scenario =>
+    scenario.flow.some(
+      ({ publishMessage = {} }) =>
+        publishMessage && 'partition' in publishMessage,
+    ),
+  )
 
   return producer
-    ? new (kafka.Producer)(client)
-    : new (kafka.HighLevelProducer)(client);
-};
-
-const getPayload = length => Buffer.alloc(length,
-  Math.random()
-    .toString(36)
-    .replace(/[^a-z]+/g, ''));
-
-const createMessage = data => (typeof data === 'object'
-  ? JSON.stringify(data)
-  : String(data));
-
-function KafkaEngine(script, ee, helpers) {
-  this.script = script;
-  this.ee = ee;
-  this.helpers = helpers;
-
-  return this;
+    ? new kafka.Producer(client)
+    : new kafka.HighLevelProducer(client)
 }
 
-KafkaEngine.prototype.createScenario = function createScenario(scenarioSpec, ee) {
-  const tasks = scenarioSpec.flow.map(rs => this.step(rs, ee));
+const getPayload = length =>
+  Buffer.alloc(
+    length,
+    Math.random()
+      .toString(36)
+      .replace(/[^a-z]+/g, ''),
+  )
 
-  return this.compile(tasks, scenarioSpec.flow, ee);
-};
+const createMessage = data =>
+  typeof data === 'object' ? JSON.stringify(data) : String(data)
+
+function KafkaEngine(script, ee, helpers) {
+  this.script = script
+  this.ee = ee
+  this.helpers = helpers
+
+  return this
+}
+
+KafkaEngine.prototype.createScenario = function createScenario(
+  scenarioSpec,
+  ee,
+) {
+  const tasks = scenarioSpec.flow.map(rs => this.step(rs, ee))
+
+  return this.compile(tasks, scenarioSpec.flow, ee)
+}
 
 KafkaEngine.prototype.step = function step(rs, ee) {
-  const self = this;
+  const self = this
 
   if (rs.loop) {
-    const steps = rs.loop.map(loopStep => this.step(loopStep, ee));
+    const steps = rs.loop.map(loopStep => this.step(loopStep, ee))
 
-    return this.helpers.createLoopWithCount(rs.count || -1, steps, {});
+    return this.helpers.createLoopWithCount(rs.count || -1, steps, {})
   }
 
   if (rs.log) {
     return function log(context, callback) {
-      logger(self.helpers.template(rs.log, context));
+      logger(self.helpers.template(rs.log, context))
 
-      return process.nextTick(() => { callback(null, context); });
-    };
+      return process.nextTick(() => {
+        callback(null, context)
+      })
+    }
   }
 
   if (rs.think) {
-    return this.helpers.createThink(rs, _.get(self.config, 'defaults.think', {}));
+    return this.helpers.createThink(
+      rs,
+      _.get(self.config, 'defaults.think', {}),
+    )
   }
 
   if (rs.publishMessage) {
     return function publishMessage(context, callback) {
-      const batchSize = Number(rs.publishMessage.batch) || 1;
-      const { producer } = context.kafka;
-      const {
-        topic, data, size, partition
-      } = rs.publishMessage;
+      const batchSize = Number(rs.publishMessage.batch) || 1
+      const { producer } = context.kafka
+      const { topic, data, size, partition } = rs.publishMessage
 
       const message = {
         topic,
         messages: new Array(batchSize)
           .fill()
-          .map(() => (data
-            ? createMessage(data)
-            : getPayload(Number(size) || DEFAULT_MESSAGE_SIZE)))
-      };
-
-      if (partition) {
-        message.partition = partition;
+          .map(() =>
+            data
+              ? createMessage(data)
+              : getPayload(Number(size) || DEFAULT_MESSAGE_SIZE),
+          ),
       }
 
-      producer.send([message], (err) => {
-        if (err) {
-          ee.emit('error', err);
-          debug(err);
+      if (partition) {
+        message.partition = partition
+      }
 
-          return callback(err, context);
+      producer.send([message], err => {
+        if (err) {
+          ee.emit('error', err)
+          debug(err)
+
+          return callback(err, context)
         }
 
-        ee.emit('response', 0, 0, context._uid);
+        ee.emit('response', 0, 0, context._uid)
 
-        return callback(null, context);
-      });
-    };
+        return callback(null, context)
+      })
+    }
   }
 
   return function s(context, callback) {
-    return callback(null, context);
-  };
-};
-
+    return callback(null, context)
+  }
+}
 
 KafkaEngine.prototype.compile = function compile(tasks, scenarioSpec, ee) {
-  const self = this;
+  const self = this
 
   return function scenario(initialContext, callback) {
     const init = function init(next) {
       if (!((self.script.config.kafka || {}).client || {}).kafkaHost) {
-        throw new Error('kafka.client.kafkaHost is required');
+        throw new Error('kafka.client.kafkaHost is required')
       }
 
-      const { kafka: { client: opts } } = self.script.config;
+      const {
+        kafka: { client: opts },
+      } = self.script.config
 
-      const kafkaClient = new kafka.KafkaClient(opts);
-      const producer = createProducer(kafkaClient, self.script.scenarios);
+      const kafkaClient = new kafka.KafkaClient(opts)
+      const producer = createProducer(kafkaClient, self.script.scenarios)
 
-      producer.on('error', (err) => {
-        ee.emit('error', err);
-      });
+      producer.on('error', err => {
+        ee.emit('error', err)
+      })
 
       producer.on('ready', () => {
-        ee.emit('started');
+        ee.emit('started')
 
-        next(null, Object.assign(initialContext, {
-          kafka: {
-            producer
-          }
-        }));
-      });
-    };
+        next(
+          null,
+          Object.assign(initialContext, {
+            kafka: {
+              producer,
+            },
+          }),
+        )
+      })
+    }
 
-    const steps = [init].concat(tasks);
+    const steps = [init].concat(tasks)
 
-    A.waterfall(
-      steps,
-      (err, context) => {
-        if (err) {
-          debug(err);
-        }
+    A.waterfall(steps, (err, context) => {
+      if (err) {
+        debug(err)
+      }
 
-        return callback(err, context);
-      },
-    );
-  };
-};
+      return callback(err, context)
+    })
+  }
+}
 
-module.exports = KafkaEngine;
+module.exports = KafkaEngine
